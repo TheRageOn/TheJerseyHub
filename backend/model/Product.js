21da3385f30025a4ee4dd938e5b4ed0d461f736b
TheRageOn <rijankarki1234567890@gmail.com>
2026-06-14 21:20:34 +0545

    feat: add product model

---
+++ backend/model/Product.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+
+// Stock available for each size
+const sizeSchema = new mongoose.Schema(
+  {
+    size: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  { _id: false },
+);
+
+// Product model
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    category: {
+      type: String,
+      enum: ["club", "nation", "special"],
+      required: true,
+    },
+
+    teamName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    discountPrice: {
+      type: Number,
+      min: 0,
+      default: 0,
+    },
+
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+
+    sizes: [sizeSchema],
+
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Product", productSchema);
